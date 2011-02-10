9284b5fe06203344caa21b21a656a343f59eacbf
DGH <DGH@bab10101-e421-0410-a517-8ce0973de3ef>
2011-02-10 16:03:26 +0000

    Plugins SVN re-organization
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-linkpages@25886 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/linkpages/core/create_linkpages_portlet.sql
@@ -0,0 +1,11 @@
+--
+-- Structure for table linkpages_portlet
+--
+
+DROP TABLE IF EXISTS linkpages_portlet;
+CREATE TABLE linkpages_portlet (
+  id_portlet INT DEFAULT '0' NOT NULL,
+  id_linkpage INT DEFAULT '0' NOT NULL,
+  linkpage_order INT DEFAULT '0',
+  PRIMARY KEY (id_portlet,id_linkpage)
+);
